d788ef81294c4634502407b7be560eec40499481
Bishnu <bdahal17@gmail.com>
2021-02-10 20:27:49 -0500

    update

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const aboutMe = document.querySelector(".aboutMeButton");
 const changingDisplayAboutMe = document.querySelector(".pop");
 const emoji = document.querySelector(".emoji");
+changingDisplayAboutMe.style.display = "none";
 aboutMe.addEventListener("click", function () {
   changingDisplayAboutMe.style.display = "flex";
 });
