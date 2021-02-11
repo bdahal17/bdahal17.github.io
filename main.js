a591e3e065ea3a121c315e1e0da6d12f3417c6f9
Bishnu <bdahal17@gmail.com>
2021-02-10 20:41:43 -0500

    updated links 2/10/22

--- main.js
+++ main.js
@@ -2,10 +2,14 @@
 const aboutMe = document.querySelector(".aboutMeButton");
 const changingDisplayAboutMe = document.querySelector(".pop");
 const emoji = document.querySelector(".emoji");
+const prompt = document.querySelector(".email");
 changingDisplayAboutMe.style.display = "none";
 aboutMe.addEventListener("click", function () {
   changingDisplayAboutMe.style.display = "flex";
 });
 emoji.addEventListener("click", function () {
   changingDisplayAboutMe.style.display = "none";
+});
+prompt.addEventListener("click", function () {
+  alert("bdahal17@gmail.com");
 });
